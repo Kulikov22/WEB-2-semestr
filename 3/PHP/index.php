8b7ea31a07c2ea5e333645e9fcbda6642258c882
Kulikov22 <144003743+Kulikov22@users.noreply.github.com>
2024-04-02 09:38:15 +0300

    Update index.php

--- 3/PHP/index.php
+++ 3/PHP/index.php
@@ -102,9 +102,9 @@ if ($errors) {
     exit();
 }
 
-$user = 'u67309';
-$pass = '1824692';
-$db = new PDO('mysql:host=localhost;dbname=u67309', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$user = '*****';
+$pass = '******';
+$db = new PDO('mysql:host=localhost;dbname=*****', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 foreach ($_POST['languages'] as $language) {
