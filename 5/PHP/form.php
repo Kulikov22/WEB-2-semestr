6a36d61f102f71aadde8e498c2e0ec7db90528ff
Kulikov22 <144003743+Kulikov22@users.noreply.github.com>
2024-05-22 22:15:57 +0300

    Update form.php

--- 5/PHP/form.php
+++ 5/PHP/form.php
@@ -24,7 +24,7 @@
     <form action="" method="POST">
       Имя:<br><input type="text" name="names" <?php if (!empty($errors['name'])) {print 'class="group error"';} else print 'class="group"'; ?> value="<?php print isset($values['name']) ? htmlspecialchars($values['name']) : ''; ?>">
       <br>
-      Телефон:<br><input type="tel" name="phone" <?php if (!empty($errors['phones'])) {print 'class="group error"';} else print 'class="group"'; ?> value="<?php print isset($values['phones']) ? htmlspecialchars($values['phones']) : ''; ?>">
+      Телефон:<br><input type="tel" name="phones" <?php if (!empty($errors['phones'])) {print 'class="group error"';} else print 'class="group"'; ?> value="<?php print isset($values['phones']) ? htmlspecialchars($values['phones']) : ''; ?>">
       <br>
       E-mail:
         <br><input type="text" name="email" <?php if (!empty($errors['email'])) {print 'class="group error"';} else print 'class="group"'; ?> value="<?php print isset($values['email']) ? htmlspecialchars($values['email']) : ''; ?>"><br>
@@ -39,16 +39,16 @@
       </div>
       Любимый язык программирования:<br>
       <select class="group" name="languages[]" size="10" multiple>
-        <option value="Pascal" <?php if (isset($values['language']) && in_array("Pascal", $values['language'])) {print 'selected';} ?>>Pascal</option>
-        <option value="C" <?php if (isset($values['language']) && in_array("C", $values['language'])) {print 'selected';} ?>>C</option>
-        <option value="C_plus_plus" <?php if (isset($values['language']) && in_array("C_plus_plus", $values['language'])) {print 'selected';} ?>>C++</option>
-        <option value="JavaScript" <?php if (isset($values['language']) && in_array("JavaScript", $values['language'])) {print 'selected';} ?>>JavaScript</option>
-        <option value="PHP" <?php if (isset($values['language']) && in_array("PHP", $values['language'])) {print 'selected';} ?>>PHP</option>
-        <option value="Python" <?php if (isset($values['language']) && in_array("Python", $values['language'])) {print 'selected';} ?>>Python</option>
-        <option value="Java" <?php if (isset($values['language']) && in_array("Java", $values['language'])) {print 'selected';} ?>>Java</option>
-        <option value="Haskel" <?php if (isset($values['language']) && in_array("Haskel", $values['language'])) {print 'selected';} ?>>Haskel</option>
-        <option value="Clojure" <?php if (isset($values['language']) && in_array("Clojure", $values['language'])) {print 'selected';} ?>>Clojure</option>
-        <option value="Prolog" <?php if (isset($values['language']) && in_array("Prolog", $values['language'])) {print 'selected';} ?>>Prolog</option>
+        <option value="Pascal" <?php if (isset($values['languages']) && in_array("Pascal", $values['languages'])) {print 'selected';} ?>>Pascal</option>
+        <option value="C" <?php if (isset($values['languages']) && in_array("C", $values['languages'])) {print 'selected';} ?>>C</option>
+        <option value="C_plus_plus" <?php if (isset($values['languages']) && in_array("C_plus_plus", $values['languages'])) {print 'selected';} ?>>C++</option>
+        <option value="JavaScript" <?php if (isset($values['languages']) && in_array("JavaScript", $values['languages'])) {print 'selected';} ?>>JavaScript</option>
+        <option value="PHP" <?php if (isset($values['languages']) && in_array("PHP", $values['languages'])) {print 'selected';} ?>>PHP</option>
+        <option value="Python" <?php if (isset($values['languages']) && in_array("Python", $values['languages'])) {print 'selected';} ?>>Python</option>
+        <option value="Java" <?php if (isset($values['languages']) && in_array("Java", $values['languages'])) {print 'selected';} ?>>Java</option>
+        <option value="Haskel" <?php if (isset($values['languages']) && in_array("Haskel", $values['languages'])) {print 'selected';} ?>>Haskel</option>
+        <option value="Clojure" <?php if (isset($values['languages']) && in_array("Clojure", $values['languages'])) {print 'selected';} ?>>Clojure</option>
+        <option value="Prolog" <?php if (isset($values['languages']) && in_array("Prolog", $values['languages'])) {print 'selected';} ?>>Prolog</option>
       </select>
       <br>
       Биография:<br><textarea class="group" name="biography" rows="3" cols="30"><?php print isset($values['biography']) ? htmlspecialchars($values['biography']) : ''; ?></textarea>
@@ -62,9 +62,9 @@
   <div class="container">
     <?php
       if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login']))
-        print('<a href="login.php" class = "enter-exit" title = "Log out">Выйти</a>');
+        print('<a href="login.php" class="enter-exit" title="Log out">Выйти</a>');
       else
-        print('<a href="login.php" class = "enter-exit"  title = "Log in">Войти</a>');
+        print('<a href="login.php" class="enter-exit" title="Log in">Войти</a>');
     ?>
   </div>
 </body>
