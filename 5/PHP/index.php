2b363beedabe42254c8bcb55361913d69eeaf4f8
Kulikov22 <144003743+Kulikov22@users.noreply.github.com>
2024-05-22 22:01:55 +0300

    Update index.php

--- 5/PHP/index.php
+++ 5/PHP/index.php
@@ -16,19 +16,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         }
     }
     $errors = array();
-    $errors['names'] = !empty($_COOKIE['name_error']);
-    $errors['phone'] = !empty($_COOKIE['phone_error']);
+    $errors['name'] = !empty($_COOKIE['name_error']);
+    $errors['phones'] = !empty($_COOKIE['phones_error']);
     $errors['email'] = !empty($_COOKIE['email_error']);
     $errors['data'] = !empty($_COOKIE['data_error']);
     $errors['gender'] = !empty($_COOKIE['gender_error']);
     $errors['agree'] = !empty($_COOKIE['agree_error']);
 
-    if ($errors['names']) {
-        setcookie('names_error', '', 100000);
+    if ($errors['name']) {
+        setcookie('name_error', '', 100000);
         $messages[] = '<div>Заполните имя.</div>';
     }
-    if ($errors['phone']) {
-        setcookie('phone_error', '', 100000);
+    if ($errors['phones']) {
+        setcookie('phones_error', '', 100000);
         $messages[] = '<div>Некорректный телефон.</div>';
     }
     if ($errors['email']) {
@@ -49,8 +49,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     }
 
     $values = array();
-    $values['names'] = isset($_COOKIE['names_value']) ? strip_tags($_COOKIE['names_value']) : '';
-    $values['phone'] = isset($_COOKIE['phone_value']) ? strip_tags($_COOKIE['phone_value']) : '';
+    $values['name'] = isset($_COOKIE['name_value']) ? strip_tags($_COOKIE['name_value']) : '';
+    $values['phones'] = isset($_COOKIE['phones_value']) ? strip_tags($_COOKIE['phones_value']) : '';
     $values['email'] = isset($_COOKIE['email_value']) ? strip_tags($_COOKIE['email_value']) : '';
     $values['data'] = isset($_COOKIE['data_value']) ? $_COOKIE['data_value'] : '';
     $values['gender'] = isset($_COOKIE['gender_value']) ? $_COOKIE['gender_value'] : '';
@@ -60,12 +60,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     session_start();
     if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) {
-        $db = new PDO('mysql:host=localhost;dbname=u67439', 'u67309', '1824692', array(PDO::ATTR_PERSISTENT => true));
+        $db = new PDO('mysql:host=localhost;dbname=u67309', 'u67309', '1824692', array(PDO::ATTR_PERSISTENT => true));
         $stmt = $db->prepare("SELECT * FROM application WHERE id = ?");
         $stmt->execute([$_SESSION['uid']]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $values['names'] = strip_tags($row['names']);
-        $values['phone'] = strip_tags($row['phone']);
+        $values['name'] = strip_tags($row['name']);
+        $values['phones'] = strip_tags($row['phones']);
         $values['email'] = strip_tags($row['email']);
         $values['data'] = $row['data'];
         $values['gender'] = $row['gender'];
@@ -80,18 +80,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 else {
     $errors = FALSE;
-    if (empty($_POST['names'])) {
-        setcookie('names_error', '1', time() + 24 * 60 * 60);
+    if (empty($_POST['name'])) {
+        setcookie('name_error', '1', time() + 24 * 60 * 60);
         $errors = TRUE;
     } else {
-        setcookie('names_value', $_POST['names'], time() + 12 * 30 * 24 * 60 * 60);
+        setcookie('name_value', $_POST['name'], time() + 12 * 30 * 24 * 60 * 60);
     }
 
-    if (!preg_match('/^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/', $_POST['phone'])) {
-        setcookie('phone_error', '1', time() + 24 * 60 * 60);
+    if (!preg_match('/^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/', $_POST['phones'])) {
+        setcookie('phones_error', '1', time() + 24 * 60 * 60);
         $errors = TRUE;
     } else {
-        setcookie('phone_value', $_POST['phone'], time() + 30 * 24 * 60 * 60);
+        setcookie('phones_value', $_POST['phones'], time() + 30 * 24 * 60 * 60);
     }
 
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
@@ -133,8 +133,8 @@ else {
         header('Location: index.php');
         exit();
     } else {
-        setcookie('names_error', '', 100000);
-        setcookie('phone_error', '', 100000);
+        setcookie('name_error', '', 100000);
+        setcookie('phones_error', '', 100000);
         setcookie('email_error', '', 100000);
         setcookie('data_error', '', 100000);
         setcookie('gender_error', '', 100000);
@@ -144,8 +144,8 @@ else {
     session_start();
     if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) {
         $db = new PDO('mysql:host=localhost;dbname=u67309', 'u67309', '1824692', array(PDO::ATTR_PERSISTENT => true));
-        $stmt = $db->prepare("UPDATE application SET names = ?, phone = ?, email = ?, data = ?, gender = ?, biography = ? WHERE id = ?");
-        $stmt->execute([$_POST['names'], $_POST['phone'], $_POST['email'], $_POST['data'], $_POST['gender'], $_POST['biography'], $_SESSION['uid']]);
+        $stmt = $db->prepare("UPDATE application SET name = ?, phones = ?, email = ?, data = ?, gender = ?, biography = ? WHERE id = ?");
+        $stmt->execute([$_POST['name'], $_POST['phones'], $_POST['email'], $_POST['data'], $_POST['gender'], $_POST['biography'], $_SESSION['uid']]);
         $stmt = $db->prepare("DELETE FROM application_languages WHERE id = ?");
         $stmt->execute([$_SESSION['uid']]);
         $stmt = $db->prepare("INSERT INTO application_languages (id, name_of_language) VALUES (?, ?)");
@@ -153,30 +153,24 @@ else {
             $stmt->execute([$_SESSION['uid'], $language]);
         }
     } else {
-        $login = 'u' . substr(uniqid(), -5);
-        $password = substr(md5(uniqid()), 0, 15); // Пароль должен быть не длиннее, чем максимальная длина в базе данных
-
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-        setcookie('login', $login);
-        setcookie('pass', $password);
-
-        try {
-            $db = new PDO('mysql:host=localhost;dbname=u67309', 'u67309', '1824692', array(PDO::ATTR_PERSISTENT => true));
-            $stmt = $db->prepare("INSERT INTO application (names, phone, email, data, gender, biography) VALUES (?, ?, ?, ?, ?, ?)");
-            $stmt->execute([$_POST['names'], $_POST['phone'], $_POST['email'], $_POST['data'], $_POST['gender'], $_POST['biography']]);
-            $id = $db->lastInsertId();
-            $stmt = $db->prepare("INSERT INTO application_languages (id, name_of_language) VALUES (?, ?)");
-            foreach ($languages as $language) {
-                $stmt->execute([$id, $language]);
-            }
-            $stmt = $db->prepare("INSERT INTO login_pass (id, login, pass) VALUES (?, ?, ?)");
-            $stmt->execute([$id, $login, $hash]);
-        } catch (PDOException $e) {
-            print('Error : ' . $e->getMessage());
-            exit();
+        $db = new PDO('mysql:host=localhost;dbname=u67309', 'u67309', '1824692', array(PDO::ATTR_PERSISTENT => true));
+        $stmt = $db->prepare("INSERT INTO application (name, phones, email, data, gender, biography) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$_POST['name'], $_POST['phones'], $_POST['email'], $_POST['data'], $_POST['gender'], $_POST['biography']]);
+        $id = $db->lastInsertId();
+        $stmt = $db->prepare("INSERT INTO application_languages (id, name_of_language) VALUES (?, ?)");
+        foreach ($languages as $language) {
+            $stmt->execute([$id, $language]);
         }
+        $login = 'user' . substr(uniqid(), -5);
+        $pass = substr(md5(uniqid()), 0, 10);
+        $hash = md5($pass);
+        $stmt = $db->prepare("INSERT INTO users (id, login, pass) VALUES (?, ?, ?)");
+        $stmt->execute([$id, $login, $hash]);
+        setcookie('login', $login);
+        setcookie('pass', $pass);
     }
     setcookie('save', '1');
     header('Location: index.php');
 }
 ?>
+
