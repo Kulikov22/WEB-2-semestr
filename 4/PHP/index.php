1b80134db810ddf5872bb47a5fc6ab29c02a213c
Kulikov22 <144003743+Kulikov22@users.noreply.github.com>
2024-06-02 18:29:27 +0300

    Update index.php

--- 4/PHP/index.php
+++ 4/PHP/index.php
@@ -81,123 +81,99 @@ $values = array();
   include('form.php');
 }
 else {
-  $errors = FALSE;
-  if (empty($_POST['names'])) {
-    setcookie('names_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-  }
-setcookie('names_value', $_POST['names'], time() + 30 * 24 * 60 * 60);
-  if (empty($_POST['phone'])) {
-    setcookie('phone_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-  }
-  setcookie('phone_value', $_POST['phone'], time() + 30 * 24 * 60 * 60);
- 
-  if (empty($_POST['email'])) {
-    setcookie('email_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-  }
-  setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
- 
-  if (empty($_POST['date'])) {
-    setcookie('date_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-  }
-  setcookie('date_value', $_POST['date'], time() + 30 * 24 * 60 * 60);
- 
-  if (empty($_POST['gender'])) {
-    setcookie('gender_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-  }
-  setcookie('gender_value', $_POST['gender'], time() + 30 * 24 * 60 * 60);
- 
-  if (empty($_POST['languages'])) {
-    setcookie('languages_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-  }
-  $language_string = implode(",", $_POST['languages']);
-  setcookie('languages_value', $language_string, time() + 30 * 24 * 60 * 60);
-  $languages_array = explode(",", $_COOKIE['languages_value']);
- 
-  if (empty($_POST['biography'])) {
-    setcookie('biography_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-  }
-  setcookie('biography_value', $_POST['biography'], time() + 30 * 24 * 60 * 60);
- 
-  if (empty($_POST['agree'])) {
-    setcookie('agree_error', '1', time() + 24 * 60 * 60);
-    $errors = TRUE;
-  }
-  setcookie('agree_value', $_POST['agree'], time() + 30 * 24 * 60 * 60);
- 
-  if ($errors) {
-    header('Location: index.php');
-    exit();
-  }
-  else {
-    setcookie('names_error', '', 100000);
-    setcookie('phone_error', '', 100000);
-    setcookie('email_error', '', 100000);
-    setcookie('date_error', '', 100000);
-    setcookie('gender_error', '', 100000);
-    setcookie('languages_error', '', 100000);
-    setcookie('biography_error', '', 100000);
-    setcookie('agree_error', '', 100000);
-  }
-$names = isset($_POST['names']) ? $_POST['names'] : '';
-$phone = isset($_POST['phone']) ? $_POST['phone'] : '';
-$email = isset($_POST['email']) ? $_POST['email'] : '';
-$date = isset($_POST['date']) ? $_POST['date'] : '';
-$gender = isset($_POST['gender']) ? $_POST['gender'] : '';
-$biography = isset($_POST['biography']) ? $_POST['biography'] : '';
-  $user = 'u67309';
+    $errors = FALSE;
+    if (empty($_POST['names'])) {
+        setcookie('names_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }
+    setcookie('names_value', $_POST['names'], time() + 30 * 24 * 60 * 60);
+    
+    if (empty($_POST['phone'])) {
+        setcookie('phone_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }
+    setcookie('phone_value', $_POST['phone'], time() + 30 * 24 * 60 * 60);
+
+    if (empty($_POST['email'])) {
+        setcookie('email_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }
+    setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
+
+    if (empty($_POST['date'])) {
+        setcookie('date_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }
+    setcookie('date_value', $_POST['date'], time() + 30 * 24 * 60 * 60);
+
+    if (empty($_POST['gender'])) {
+        setcookie('gender_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }
+    setcookie('gender_value', $_POST['gender'], time() + 30 * 24 * 60 * 60);
+
+    if (empty($_POST['languages'])) {
+        setcookie('languages_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }
+    $language_string = implode(",", $_POST['languages']);
+    setcookie('languages_value', $language_string, time() + 30 * 24 * 60 * 60);
+
+    if (empty($_POST['biography'])) {
+        setcookie('biography_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }
+    setcookie('biography_value', $_POST['biography'], time() + 30 * 24 * 60 * 60);
+
+    if (empty($_POST['agree'])) {
+        setcookie('agree_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }
+    setcookie('agree_value', $_POST['agree'], time() + 30 * 24 * 60 * 60);
+
+    if ($errors) {
+        header('Location: index.php');
+        exit();
+    } else {
+        setcookie('names_error', '', 100000);
+        setcookie('phone_error', '', 100000);
+        setcookie('email_error', '', 100000);
+        setcookie('date_error', '', 100000);
+        setcookie('gender_error', '', 100000);
+        setcookie('languages_error', '', 100000);
+        setcookie('biography_error', '', 100000);
+        setcookie('agree_error', '', 100000);
+    }
+
+    $names = $_POST['names'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $date = $_POST['date'];
+    $gender = $_POST['gender'];
+    $biography = $_POST['biography'];
+    $user = 'u67309';
     $pass = '1824692';
     $db = new PDO('mysql:host=localhost;dbname=u67309', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
-    foreach ($_POST['languages'] as $language) {
-        $stmt = $db->prepare("SELECT id FROM languages WHERE id= :id");
-        $stmt->bindParam(':id', $language);
-        $stmt->execute();
-        if ($stmt->rowCount() == 0) {
-          print('Ошибка при добавлении языка.<br/>');
-          exit();
-        }
-    }
- 
+
     try {
         $stmt = $db->prepare("INSERT INTO application (names,phones,email,dates,gender,biography)" . "VALUES (:names,:phone,:email,:date,:gender,:biography)");
         $stmt->execute(array('names' => $names, 'phone' => $phone, 'email' => $email, 'date' => $date, 'gender' => $gender, 'biography' => $biography));
         $applicationId = $db->lastInsertId();
-      
-        foreach ($_POST['languages'] as $language) {
-            $stmt = $db->prepare("SELECT id FROM languages WHERE id = :id");
-            $stmt->bindParam(':id', $language);
+
+        foreach ($_POST['languages'] as $languageId) {
+            $stmt = $db->prepare("INSERT INTO application_languages (id_lang, id_app) VALUES (:languageId, :applicationId)");
+            $stmt->bindParam(':languageId', $languageId);
+            $stmt->bindParam(':applicationId', $applicationId);
             $stmt->execute();
-            $languageRow = $stmt->fetch(PDO::FETCH_ASSOC);
-        
-            if ($languageRow) {
-                $languageId = $languageRow['id'];
-        
-                $stmt = $db->prepare("INSERT INTO application_languages (id_lang, id_app) VALUES (:languageId, :applicationId)");
-                $stmt->bindParam(':languageId', $languageId);
-                $stmt->bindParam(':applicationId', $applicationId);
-                $stmt->execute();
-            } else {
-                print('Ошибка: Не удалось найти ID для языка программирования: ' . $language . '<br/>');
-                exit();
-            }
         }
-            
+
         print('Спасибо, форма сохранена.');
-    }
- 
-    catch(PDOException $e){
+    } catch (PDOException $e) {
         print('Error : ' . $e->getMessage());
         exit();
     }
-  setcookie('save', '1');
- 
-  header('Location: index.php');
+
+    setcookie('save', '1');
+    header('Location: index.php');
 }
